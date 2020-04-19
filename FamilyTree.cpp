92cfe7d4a9fe6070a3b08b7fb3def70e7e006387
UrielAbergel <myfliper2@gmail.com>
2020-04-19 13:04:01 +0300

    fix the find function

--- FamilyTree.cpp
+++ FamilyTree.cpp
@@ -158,7 +158,7 @@ string Tree::find(const string reletion )
              temp = (temp) / 6;
              for (int i = 0; i < reletion.size(); ++i)
              {
-                if(reletion[i] < 97 || reletion[i] > 122 || reletion[i] != 45) throw out_of_range("not good reletion");
+                if((reletion[i] < 97 || reletion[i] > 122) && reletion[i] != 45) throw out_of_range("not good reletion");
              }
              count = temp + 2;
              sex = 2;
@@ -172,7 +172,7 @@ string Tree::find(const string reletion )
         {
             for (int i = 0; i < reletion.size(); ++i)
             {
-                if(reletion[i] < 97 || reletion[i] > 122 || reletion[i] != 45) throw out_of_range("not good reletion");
+                if((reletion[i] < 97 || reletion[i] > 122) && reletion[i] != 45) throw out_of_range("not good reletion");
             }
             temp = (temp) / 6;
             count = temp + 2;
